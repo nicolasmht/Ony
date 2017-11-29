ea3ff0d1501db3784d17f20773b437af0f6f7f48
Nicolas Michot <nicolas.michot@yahoo.com>
2017-11-29 18:54:32 +0100

    Create project

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id="app">
+
+        <header id="header">
+            <h1 id="title">Relax</h1>
+            <p id="subTitle">Mix different sounds and create your perfect environment.</p>
+        </header>
+
+        <sounds></sounds>
+
+    </div>
+</template>
+
+<script>
+
+    // Import Bootstrap
+    import 'bootstrap/dist/css/bootstrap.css'
+    import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+    // Import components
+    import Sounds from './components/sounds'
+
+    export default {
+        name: 'app',
+        components: {
+            Sounds
+        }
+    }
+</script>
+
+<style>
+
+    @import url('https://fonts.googleapis.com/css?family=Roboto:400,900');
+
+    #app{
+        position: relative;
+        width: 720px;
+        margin: auto;
+        font-family: 'Roboto', sans-serif;
+        background-color: rgba(255, 0, 0, .1);
+    }
+
+    #header{
+        padding-top: 25px;
+        margin-bottom: 50px;
+    }
+
+    #title{
+        text-align: center;
+        font-size: 32px;
+        color: #444
+    }
+
+    #subTitle{
+        text-align: center;
+        color: #666;
+    }
+</style>
+
+
---
+++ src/components/sounds.vue
@@ -0,0 +1,81 @@
+<template>
+
+    <div id="sounds">
+
+        <!-- Grid Bootstrap -->
+        <b-row>
+
+            <!-- First column -->
+            <b-col>
+                <div class="sound">
+                    <img src="http://placehold.it/200x160" alt="" class="icon-sound">
+                    <input type="range" min="0" max="100" class="range-sound">
+                </div>
+            </b-col>
+
+            <!-- Second column -->
+            <b-col>
+                <div class="sound">
+                    <img src="http://placehold.it/200x160" alt="" class="icon-sound">
+                    <input type="range" min="0" max="100" class="range-sound">
+                </div>
+
+                <div class="sound">
+                    <img src="http://placehold.it/200x160" alt="" class="icon-sound">
+                    <input type="range" min="0" max="100" class="range-sound">
+                </div>
+            </b-col>
+
+        </b-row>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: 'sounds'
+    }
+</script>
+
+<style>
+    .sound{
+        width: 200px;
+        min-height: 190px;
+        margin: auto;
+        margin-bottom: 50px;
+        /* background-color: red; */
+    }
+
+    .img-sound{
+        width: 100%;
+    }
+
+    .range-sound{
+        width: 100%;
+        height: 3px;
+
+        -webkit-appearance: none;
+        appearance: none;
+        background: #000;
+        border-radius: 2px;
+        outline: none;
+    }
+
+    .range-sound::-webkit-slider-thumb{
+        -webkit-appearance: none;
+        appearance: none;
+        width: 20px;
+        height: 20px;
+        background: #000;
+        border-radius: 10px;
+        cursor: pointer;
+    }
+
+    .range-sound::-moz-range-thumb{
+        width: 20px;
+        height: 20px;
+        background: #000;
+        border-radius: 10px;
+        cursor: pointer;
+    }
+</style>
